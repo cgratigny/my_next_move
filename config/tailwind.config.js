9961066218a5519892fc90f96fe129d4cf1cec30
Chris Gratigny <cgratigny>
2023-12-22 21:25:36 -0800

    Update the tailwind configuration to pickup changes to the simpleform configuration.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}',
     './app/components/**/*.{erb,haml,html,slim,rb}',
     './app/enums/*.rb',
+    './config/initializers/simple_form_tailwind.rb'
   ],
   theme: {
     screens: {
