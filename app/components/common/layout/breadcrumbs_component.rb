a5c3dedbe96cdc2fbfd069aacbeea083aaea94f9
Chris Gratigny <cgratigny>
2023-12-24 18:08:43 -0800

    Hide the breadcrumb trail for guest users.

--- app/components/common/layout/breadcrumbs_component.rb
+++ app/components/common/layout/breadcrumbs_component.rb
@@ -5,4 +5,9 @@ class Common::Layout::BreadcrumbsComponent < ApplicationComponent
   def breadcrumb_trail
     helpers.breadcrumb_trail
   end
+
+  def render?
+    current_user.present?
+  end
+
 end
