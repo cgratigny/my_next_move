1c8d8cc166c803750201b5b9db5d1b3363e88256
Chris Gratigny <cgratigny>
2024-01-20 20:55:42 -0800

    Update mobile styles.

--- app/components/common/layout/navigation_component.rb
+++ app/components/common/layout/navigation_component.rb
@@ -9,13 +9,8 @@ class Common::Layout::NavigationComponent < ApplicationComponent
     [
       ["Dashboard", [:dashboard]],
       ["Opportunities", [:opportunities]],
-      ["New", [:new, :opportunity]],
       ["Moves", [:moves]],
       ["Stats", [:daily_opportunity_stats]]
-      # [ "Companies", [:companies] ],
-      # [ "Notes", [:notes] ],
-      # [ "Tasks", [:tasks] ],
-      # [ "Documents", [:documents] ],
     ]
   end
 end
